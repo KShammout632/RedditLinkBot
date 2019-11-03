d33b203560a2c97bc74b4593e28adc4c1ab0890a
Kareem Shammout <kareemshammout632@gmail.com>
2019-11-02 17:37:53 -0700

    Create config.py
    Requirement for bot to login

---
+++ config.py
@@ -0,0 +1,4 @@
+username = ""       # account username
+password = ""       # account password
+client_id = ""      # client_id and client_secret can be found/generated on 
+client_secret = ""  # the "create application" page on Reddit
